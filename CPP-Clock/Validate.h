d3977b18e5d19bf817b3462d3665ef94a5f81231
thenewcoder2010 <srivastavaaryan2010@gmail.com>
2022-07-16 15:28:43 +0530

    main.cpp is able get the user input and check if it's valid. However, it is unable to check whether the date is valid

---
+++ CPP-Clock/Validate.h
@@ -0,0 +1,12 @@
+//
+//  Validate.h
+//  CPP-Clock
+//
+//  Created by Aryan Srivastava on 16/7/22.
+//
+
+#ifndef Validate_h
+#define Validate_h
+
+
+#endif /* Validate_h */
--- CPP-Clock/main.cpp
+++ CPP-Clock/main.cpp
@@ -6,9 +6,35 @@
 //
 
 #include <iostream>
+#include <string>
+#include "Validate.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    int day;
+    std::string month;
+    int year;
+    
+    std::cout << "Enter the day: ";
+    std::cin >> day;
+    
+    std::cout << "Enter the month: ";
+    std::cin >> month;
+    
+    std::cout << "Enter the year: ";
+    std::cin >> year;
+    
+    bool valid = validate(day, month, year);
+    
+    std::cout << "Valid has a value of: " << valid << "\n";
+    
+    if (valid) {
+        std::cout << "It is valid";
+    }
+    
+    else {
+        std::cout << "It is not valid. Please re-run the program!";
+    }
+    
     return 0;
 }
